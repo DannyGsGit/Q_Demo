90c0d342417c4d621284f5392c3891f3c94252d4
DannyGsGit <danny.godbout@gmail.com>
2017-02-03 15:46:38 -0800

    Began display class of environment, added requirements.txt

--- Furemu/environment_furemu.py
+++ Furemu/environment_furemu.py
@@ -14,9 +14,11 @@ class frame(object):
         pass
     
     def state(self):
+        # State of the frame with part placements (+ weights?)
         pass
     
     def apply_action(self, action_input):
+        # Move & place parts on frame according to actions
         pass
     
     
@@ -27,6 +29,7 @@ class frame(object):
     
 class parts_bin(object):
     def __init__(self, bom_name, bom_directory = 'Environment/BoMs/', part_directory = 'Environment/Parts/'):
+        # We will initialize the parts bin with a BoM (i.e. heuristic equivalent of gathering parts for a build according to BoM)
         
         # Get the parts bom from file
         with open(os.path.join(bom_directory, bom_name), 'r') as f:
@@ -52,14 +55,13 @@ class parts_bin(object):
                 
         exit(1)
 
-
-
-
     
-    def state(self):
+    def bin_state(self):
+        # Returns the contents of the parts_bin
         pass
     
-    def get_part(self):
+    def take_part_from_bin(self):
+        # This function will take the top part from the parts_bin and remove it from the list
         pass
     
 
@@ -70,11 +72,11 @@ class parts_bin(object):
 
 class action_map(object):
     def __init__(self):
-        # Define action set
-        
+        # Define action set: left, right, up, down, flip_x, flip_y, place
         pass
     
     def get_action(self, input):
+        # Map inputs to actions
         pass
     
 
@@ -87,16 +89,20 @@ class action_map(object):
 
 class reward(object):
     def __init__(self):
-        # Reward = 0
-        pass
+        # Initialize with reward = 0
+        self.reward = 0
+        exit(1)
     
     def score_violations (self, frame_state):
+        # Penalize the score if a constraint is violated
         pass
     
     def score_blue_boc(self, frame_state):
+        # Give points for placing blue parts correctly
         pass
     
     def score_red_boc(self, frame_state):
+        # Give points for placing red parts correctly
         pass
     
 
@@ -110,13 +116,61 @@ class reward(object):
 class display(object):
     def __init__(self):
         # Create display window
-        pass
+        import pygame
+
+        pygame.init()
+
+        # Define the colors we will use in RGB format
+        black = (  0,   0,   0)
+        white = (255, 255, 255)
+        blue =  (  0,   0, 255)
+        green = (  0, 255,   0)
+        red =   (255,   0,   0)
+        color_background = (66, 244, 226)
+         
+        # Set the height and width of the screen
+        size = [800, 600]
+        screen = pygame.display.set_mode(size)
+         
+        pygame.display.set_caption("Hirachi Furemu Environment")
+        
+        #Loop until the user clicks the close button.
+        done = False
+        clock = pygame.time.Clock()
+         
+        while not done:
+         
+            # This limits the while loop to a max of 10 times per second.
+            # Leave this out and we will use all CPU we can.
+            clock.tick(10)
+             
+            for event in pygame.event.get(): # User did something
+                if event.type == pygame.QUIT: # If user clicked close
+                    done=True # Flag that we are done so we exit this loop
+         
+            # All drawing code happens after the for loop and but
+            # inside the main while done==False loop.
+             
+            # Clear the screen and set the screen background
+            screen.fill(color_background)
+            
+            # This MUST happen after all the other drawing commands.
+            pygame.display.flip()
+            
+        pygame.quit()
+ 
+        
+        exit(1)
     
     def render_frame(self, frame_state):
+        # Render the current frame state
         pass
     
     def render_parts_bin(self, part_bin_state):
+        # Render the current bin state
         pass
     
     
+
+
 
