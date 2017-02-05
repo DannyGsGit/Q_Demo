839c5a361731930216ba74dcbe3c5f8658f26e5e
Unknown <danny.godbout@gmail.com>
2017-02-05 12:53:04 -0800

    Pymunk addition

--- Furemu/environment_furemu.py
+++ Furemu/environment_furemu.py
@@ -115,52 +115,7 @@ class reward(object):
 
 class display(object):
     def __init__(self):
-        # Create display window
-        import pygame
-
-        pygame.init()
-
-        # Define the colors we will use in RGB format
-        black = (  0,   0,   0)
-        white = (255, 255, 255)
-        blue =  (  0,   0, 255)
-        green = (  0, 255,   0)
-        red =   (255,   0,   0)
-        color_background = (66, 244, 226)
-         
-        # Set the height and width of the screen
-        size = [800, 600]
-        screen = pygame.display.set_mode(size)
-         
-        pygame.display.set_caption("Hirachi Furemu Environment")
-        
-        #Loop until the user clicks the close button.
-        done = False
-        clock = pygame.time.Clock()
-         
-        while not done:
-         
-            # This limits the while loop to a max of 10 times per second.
-            # Leave this out and we will use all CPU we can.
-            clock.tick(10)
-             
-            for event in pygame.event.get(): # User did something
-                if event.type == pygame.QUIT: # If user clicked close
-                    done=True # Flag that we are done so we exit this loop
-         
-            # All drawing code happens after the for loop and but
-            # inside the main while done==False loop.
-             
-            # Clear the screen and set the screen background
-            screen.fill(color_background)
-            
-            # This MUST happen after all the other drawing commands.
-            pygame.display.flip()
-            
-        pygame.quit()
- 
-        
-        exit(1)
+        pass
     
     def render_frame(self, frame_state):
         # Render the current frame state
